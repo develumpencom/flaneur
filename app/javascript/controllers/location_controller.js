7948fe87b1b3cb5dcaedf21d727ecee1665a5a44
develumpen <develumpen@breakabletoys.com>
2025-02-15 18:09:19 -0600

    broadcast new locations

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -5,10 +5,26 @@ export default class extends Controller {
   static targets = ["location"]
   static values = {
     lat: Number,
-    lon: Number
+    lon: Number,
+    broadcasted: Boolean
+  }
+
+  connect() {
+    if (this.broadcastedValue) {
+      this.removeLastLocation()
+    }
+  }
+
+  disconnect() {
   }
 
   selectLocation() {
     this.dispatch("selectLocation", { detail: { location: this } })
+  }
+
+  removeLastLocation() {
+    const locations = Array.from([...document.querySelectorAll(".location")])
+    const locationToRemove = locations.pop()
+    locationToRemove.remove()
   }
 }
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,10 @@
 class Location < ApplicationRecord
   validates_presence_of :lat, :lon, :tid, :tst
 
+  after_create_commit -> {
+    broadcast_prepend_to :locations, target: "locations", partial: "owntracks/location_row", locals: { location: self, broadcasted: true }
+  }
+
   def battery_status
     {
       0 => "unknown",
